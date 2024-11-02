d2700745ec5a7e30a58338b63ddb5c2138cf260b
Luana <luanamaistro@gmail.com>
2024-11-02 00:25:24 -0300

    feat: create DbContext and Student model

---
+++ BackEnd/OrbitaChallengeBackEnd/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrbitaChallengeBackEnd.Models;
+using System.Collections.Generic;
+
+namespace OrbitaChallengeBackEnd.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Student> Students { get; set; }
+}
+
