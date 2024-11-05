53c94c439f81404184c297b83fc9d9370ec602fc
Luana <luanamaistro@gmail.com>
2024-11-05 20:55:22 -0300

    feat: configuring router

---
+++ FrontEnd/orbita-challenge-frontend/src/router/index.js
@@ -0,0 +1,32 @@
+// src/router/index.js
+
+import { createRouter, createWebHistory } from 'vue-router';
+import AcademicModule from '@/views/AcademicModule.vue';
+import EditStudent from '@/views/EditStudent.vue';
+import AddStudent from '@/views/AddStudent.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'AcademicModule',
+    component: AcademicModule,
+  },
+  {
+    path: '/edit/:id', // Rota para editar aluno
+    name: 'EditStudent',
+    component: EditStudent,
+  },
+  {
+    path: '/AddStudent', // Rota para editar aluno
+    name: 'AddStudent',
+    component: AddStudent,
+  },
+ 
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
